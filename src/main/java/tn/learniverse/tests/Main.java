4725913ceaadf8762486ba2b02fc5f412dad60ea
FaroukBentaleb <farouk.bentaleb@esprit.tn>
2025-04-24 18:05:19 +0100

    Resolved config issues

--- src/main/java/tn/learniverse/tests/Main.java
+++ src/main/java/tn/learniverse/tests/Main.java
@@ -7,17 +7,5 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
-        UserService userService = new UserService();
-<<<<<<< HEAD
-        User usr = new User("adem", "abidi", "adem@gmail.com","12345678", "1980-05-01", 123456789, "Instructor", "Experienced Instructor", 5, "Instructor", "resume.pdf", "profile.jpg", "facebook.com/adem", "instagram.com/adem", "linkedin.com/in/adem", true, 10, 0);
-=======
-        User usr = new User("adem", "abidi", "adem@gmail.com","Admin","12345678", "1980-05-01", 123456789, "Instructor", "Experienced Instructor", 5, "Instructor", "resume.pdf", "profile.jpg", "facebook.com/adem", "instagram.com/adem", "linkedin.com/in/adem", true, 10, 0);
->>>>>>> 6dd8374487465a91746bd900ce966fd6232155cd
-        try{
-            System.out.println(userService.getAllUsers());
-        }
-        catch(SQLException e){
-            System.out.println(e.getMessage());
-        }
     }
 }
